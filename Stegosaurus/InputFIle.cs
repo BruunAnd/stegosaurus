094378ddc64c6f85520f52e49217a8163bd6f852
andersjkbsn <andersjkbsn@gmail.com>
2016-03-31 12:12:21 +0200

    added base classes

---
+++ Stegosaurus/InputFIle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stegosaurus
+{
+    class InputFile
+    {
+        public string Name;
+        public byte[] Content;
+    }
+}
---
+++ Stegosaurus/StegoAlgorithm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stegosaurus
+{
+    abstract class StegoAlgorithm
+    {
+
+        public StegoCarrier Carrier { get; set; }
+
+        public abstract long ComputeBandwidth();
+
+        public abstract void Embed(StegoMessage message);
+
+        public abstract StegoMessage Extract();
+    }
+}
