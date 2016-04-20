32b5f81bc9a73a2aaf809ff878b3336c6c2a2d00
andersjkbsn <andersjkbsn@gmail.com>
2016-04-20 13:16:57 +0200

    added samples per vertex, graph theory skeleton

---
+++ Stegosaurus/Algorithm/GraphTheoreticAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stegosaurus.Carrier;
+
+namespace Stegosaurus.Algorithm
+{
+    public class GraphTheoreticAlgorithm : IStegoAlgorithm
+    {
+        public ICarrierMedia CarrierMedia
+        {
+            get; set;
+        }
+
+        public byte[] Key
+        {
+            get; set;
+        }
+
+        public string Name => "Graph Theoretic Algorithm";
+
+        public long ComputeBandwidth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Embed(StegoMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StegoMessage Extract()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
