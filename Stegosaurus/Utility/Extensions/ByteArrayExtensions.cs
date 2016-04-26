7fdb23cd83858564f931d01e133f0a868ebdc97a
andersjkbsn <andersjkbsn@gmail.com>
2016-04-26 14:11:11 +0200

    fixed seed (hopefully)

---
+++ Stegosaurus/Utility/Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Stegosaurus.Utility.Extensions
+{
+    public static class ByteArrayExtensions
+    {
+        public static int ComputeHash(this byte[] _array)
+        {
+            if (_array == null)
+                return 0;
+
+            int hash = 17;
+
+            foreach (byte value in _array)
+                hash = hash * 23 + value.GetHashCode();
+
+            return hash;
+        }
+    }
+}
