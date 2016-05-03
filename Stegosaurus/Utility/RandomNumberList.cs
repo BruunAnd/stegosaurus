b76082ce26789645d4080f3ccfa771189349820a
andersjkbsn <andersjkbsn@gmail.com>
2016-05-03 10:30:04 +0200

    updated tests

--- Stegosaurus/Utility/RandomNumberList.cs
+++ Stegosaurus/Utility/RandomNumberList.cs
@@ -23,7 +23,9 @@ namespace Stegosaurus.Utility
 
             // Check if there are any more integers to generate
             if (generatedIntegers.Count >= maxValue)
+            {
                 throw new ArgumentOutOfRangeException("No more integers to generate.");
+            }
 
             // Generate an integer which has not yet been generated
             int requestedCount = generatedIntegers.Count + 1;
---
+++ StegosaurusTest/LSBTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stegosaurus.Utility;
+using System.Linq;
+
+namespace StegosaurusTest
+{
+    [TestClass]
+    public class RandomNumberListTest
+    {
+        [TestMethod]
+        public void GenerateNumbers_ThrowsOutOfRangeException()
+        {
+            const int count = 1000;
+            new RandomNumberList(0, count).Take(count + 1);
+        }
+
+        [TestMethod]
+        public void GenerateNumbers_NoDuplicates()
+        {
+            const int count = 1000;
+            int duplicateCount = 0;
+            int[] randomInts = new RandomNumberList(0, count).Take(1000).ToArray();
+
+            // check for duplicates
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else if (randomInts[i] == randomInts[j])
+                    {
+                        duplicateCount++;
+                    }
+                }
+            }
+
+            Assert.Equals(duplicateCount, 0);
+        }
+    }
+}
