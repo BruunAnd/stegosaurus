1aade60548600baaf76f29974f3a58add19ddf53
andersjkbsn <andersjkbsn@gmail.com>
2016-05-12 14:43:10 +0200

    added underscore fuck matias xd

--- Stegosaurus/Utility/SizeFormatter.cs
+++ Stegosaurus/Utility/SizeFormatter.cs
@@ -10,12 +10,12 @@ namespace Stegosaurus.Utility
         /// <summary>
         /// Returns a formatted string from a byte count.
         /// </summary>
-        public static string StringFormatBytes(long byteCount)
+        public static string StringFormatBytes(long _byteCount)
         {
             string[] suffixes = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB" };
             int logIndex = 0;
             const int order = 1024;
-            decimal decByteCount = byteCount;
+            decimal decByteCount = _byteCount;
 
             for (logIndex = 0; decByteCount >= order || decByteCount <= -order; logIndex++)
             {
