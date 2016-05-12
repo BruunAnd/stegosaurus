bdb5e5a38d54a134b52da04065c4e7618e8bd524
Matias Jensen <sejemus@yahoo.dk>
2016-05-12 14:37:55 +0200

    Added summaries to InputFile.cs and StegoMessage.cs
    Title says it all :dancers:

--- Stegosaurus/InputFIle.cs
+++ Stegosaurus/InputFIle.cs
@@ -5,15 +5,27 @@ namespace Stegosaurus
 {
     public class InputFile
     {
+        /// <summary>
+        /// The name of the file.
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// A byte array containing the files contents.
+        /// </summary>
         public byte[] Content { get; }
 
+        /// <summary>
+        /// A constructor to construct a InputFile from byte array.
+        /// </summary>
         public InputFile(string _name, byte[] _content)
         {
             Name = _name;
             Content = _content;
         }
 
+        /// <summary>
+        /// A constructor to construct a InputFile from a file path.
+        /// </summary>
         public InputFile(string _filePath)
         {
             FileInfo fileInfo = new FileInfo(_filePath);
@@ -24,6 +36,9 @@ namespace Stegosaurus
             Content = File.ReadAllBytes(_filePath);
         }
 
+        /// <summary>
+        /// This method is used to save the file in the file system of the operating system.
+        /// </summary>
         public void SaveTo(string _destination)
         {
             File.WriteAllBytes(_destination, Content);
--- Stegosaurus/StegoMessage.cs
+++ Stegosaurus/StegoMessage.cs
@@ -21,39 +21,57 @@ namespace Stegosaurus
             Signed = 0x8,
         }
 
+        /// <summary>
+        /// This is the text message that can be saved in the StegoMessage.
+        /// </summary>
         public string TextMessage { get; set; }
+        /// <summary>
+        /// This is where each file will be stored in the StegoMessage.
+        /// </summary>
         public List<InputFile> InputFiles { get; } = new List<InputFile>();
 
         public StegoMessageFlags Flags;
 
+        /// <summary>
+        /// TODO: Write summary.
+        /// </summary>
         public string PrivateSigningKey { get; set; }
 
-        public StegoMessage()
+        /// <summary>
+        /// Empty constructor.
+        /// </summary>
+        public StegoMessage ()
         {
         }
 
-        public StegoMessage(string _textMessage)
+        /// <summary>
+        /// This constructer is used if only a text message is applied.
+        /// </summary>
+        public StegoMessage (string _textMessage)
         {
             TextMessage = _textMessage;
         }
 
+        /// <summary>
+        /// This constructer takes a byte array containing the data to add to the StegoMessage.
+        /// </summary>
         public StegoMessage(byte[] _fromArray, ICryptoProvider _cryptoProvider = null)
         {
             using (MemoryStream inputStream = new MemoryStream(_fromArray))
             {
                 Flags = (StegoMessageFlags) inputStream.ReadByte();
 
-                // Read encoded data
+                // Read encoded data.
                 byte[] encodedData = inputStream.ReadBytes();
                 byte[] hash = inputStream.ReadBytes();
 
-                // Verify hash of encoded data
+                // Verify hash of encoded data.
                 if (!encodedData.ComputeSHAHash().SequenceEqual(hash))
                 {
                     throw new StegoMessageException("Hash does not match, data is corrupt.");
                 }
 
-                // Decrypt if a key is specified
+                // Decrypt if a key is specified.
                 if (Flags.HasFlag(StegoMessageFlags.Encrypted) && _cryptoProvider != null)
                 {
                     try
@@ -66,13 +84,13 @@ namespace Stegosaurus
                     }
                 }
 
-                // Decompress the array
+                // Decompress the array.
                 if (Flags.HasFlag(StegoMessageFlags.Compressed))
                 {
                     encodedData = Compression.Decompress(encodedData);
                 }
 
-                // Decode array
+                // Decode array.
                 if (Flags.HasFlag(StegoMessageFlags.Encoded))
                 {
                     Decode(encodedData);
@@ -80,34 +98,40 @@ namespace Stegosaurus
             }
         }
 
+        /// <summary>
+        /// This method takes a byte array and retrieves the contents and adds it to the StegoMessage.
+        /// </summary>
         private void Decode(byte[] _byteArray)
         {
             using (MemoryStream tempStream = new MemoryStream(_byteArray))
             {
-                // Read input files
+                // Read input files.
                 int numberOfFiles = tempStream.ReadInt();
                 for (int i = 0; i < numberOfFiles; i++)
                 {
                     InputFiles.Add(tempStream.ReadInputFile());
                 }
 
-                // Read text message
+                // Read text message.
                 TextMessage = tempStream.ReadString();
             }
         }
 
+        /// <summary>
+        /// This method returns a byte array representing the data within the StegoMessage.
+        /// </summary>
         private byte[] Encode()
         {
             using (MemoryStream tempStream = new MemoryStream())
             {
-                // Write input files
+                // Write input files.
                 tempStream.Write(InputFiles.Count);
                 foreach (InputFile inputFile in InputFiles)
                 {
                     tempStream.Write(inputFile);
                 }
 
-                // Write text message
+                // Write text message.
                 tempStream.Write(TextMessage);
 
                 return tempStream.ToArray();
@@ -121,11 +145,11 @@ namespace Stegosaurus
         /// </summary>
         public byte[] ToByteArray(ICryptoProvider _cryptoProvider = null)
         {
-            // Encode and compress array
+            // Encode and compress array.
             byte[] encodedData = Encode();
             SetFlag(StegoMessageFlags.Encoded, true);
 
-            // Compress data
+            // Compress data.
             byte[] compressedData = Compression.Compress(encodedData);
             if (compressedData.Length < encodedData.Length)
             {
@@ -137,7 +161,7 @@ namespace Stegosaurus
                 SetFlag(StegoMessageFlags.Compressed, false);
             }
 
-            // Encrypt if key is specified
+            // Encrypt if key is specified.
             if (_cryptoProvider != null && _cryptoProvider.Key != null)
             {
                 encodedData = _cryptoProvider.Encrypt(encodedData);
@@ -148,22 +172,22 @@ namespace Stegosaurus
                 SetFlag(StegoMessageFlags.Encrypted, false);
             }
 
-            // Combine all the data using a MemoryStream
+            // Combine all the data using a MemoryStream.
             using (MemoryStream tempStream = new MemoryStream())
             {
-                // Do not write at the beginning of the stream
-                // Allocate some space for the int that contains size
+                // Do not write at the beginning of the stream.
+                // Allocate some space for the int that contains size.
                 tempStream.Seek(sizeof(int), SeekOrigin.Begin);
                 tempStream.WriteByte((byte) Flags);
                 tempStream.Write(encodedData, true);
                 tempStream.Write(encodedData.ComputeSHAHash(), true);
                 if (!string.IsNullOrEmpty(PrivateSigningKey))
                 {
-                    // TODO add signing stuff here
+                    // TODO add signing stuff here.
                     SetFlag(StegoMessageFlags.Signed, true);    
                 }
 
-                // Go back to beginning of stream and write length
+                // Go back to beginning of stream and write length.
                 tempStream.Seek(0, SeekOrigin.Begin);
                 tempStream.Write((int)tempStream.Length - sizeof(int));
 
@@ -171,16 +195,22 @@ namespace Stegosaurus
             }
         }
 
+        /// <summary>
+        /// This method is used to set a flag to indicate what operations has been done to the StegoMessage.
+        /// </summary>
         private void SetFlag(StegoMessageFlags _flag, bool _state)
         {
-            // Check if current flag state does not equal wanted state
+            // Check if current flag state does not equal wanted state.
             if (_state != Flags.HasFlag(_flag))
             {
-                // Flip state
+                // Flip state.
                 Flags ^= _flag;
             }
         }
 
+        /// <summary>
+        /// This method returns the compressed size of the data stored in this StegoMessage.
+        /// </summary>
         public long GetCompressedSize()
         {
             return ToByteArray().Length;
