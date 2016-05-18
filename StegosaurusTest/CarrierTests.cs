a25cf5d7af470aadcdaa25f7d1b0e1330b79966a
Anders <andersjkbsn@gmail.com>
2016-05-19 01:15:44 +0200

    updated tests

--- StegosaurusTest/CarrierTests.cs
+++ StegosaurusTest/CarrierTests.cs
@@ -12,18 +12,22 @@ namespace StegosaurusTest
     public class CarrierTests
     {
         [TestMethod]
-        public void ImageCarrier_SaveLoad_ExpectedOut()
+        public void ImageCarrier_SaveLoad_ExpectedOutput()
         {
-            Random rand = new Random();
-            ImageCarrier carrierMedia = new ImageCarrier(new Bitmap(rand.Next(500, 1000), rand.Next(500, 1000)));
-            byte[] tempArray = new byte[carrierMedia.InnerImage.Width * carrierMedia.InnerImage.Height * carrierMedia.BytesPerSample];
-            rand.NextBytes(tempArray);
+            for (int i = 0; i < 5; i++)
+            {
+                Random rand = new Random();
+                ImageCarrier carrierMedia = new ImageCarrier(new Bitmap(rand.Next(5, 2000), rand.Next(5, 2000)));
+                byte[] tempArray = new byte[carrierMedia.InnerImage.Width * carrierMedia.InnerImage.Height * carrierMedia.BytesPerSample];
+                Console.WriteLine("Trying a {0}x{1} image..", carrierMedia.InnerImage.Width, carrierMedia.InnerImage.Height);
+                rand.NextBytes(tempArray);
 
-            carrierMedia.ByteArray = tempArray;
-            carrierMedia.SaveToFile("out.png");
+                carrierMedia.ByteArray = tempArray;
+                carrierMedia.SaveToFile("out.png");
 
-            ImageCarrier reloadedCarrier = new ImageCarrier("out.png");
-            Assert.IsTrue(tempArray.SequenceEqual(reloadedCarrier.ByteArray));
+                ImageCarrier reloadedCarrier = new ImageCarrier("out.png");
+                Assert.IsTrue(tempArray.SequenceEqual(reloadedCarrier.ByteArray));
+            }
         }
     }
 }
