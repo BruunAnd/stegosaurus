09a368325582750fbc6ad6bb778cfc8160f7c52b
andersjkbsn <andersjkbsn@gmail.com>
2016-05-19 10:51:09 +0200

    icon changes

--- Stegosaurus/Forms/FormEmbeddingProgress.Designer.cs
+++ Stegosaurus/Forms/FormEmbeddingProgress.Designer.cs
@@ -33,6 +33,7 @@
             this.labelStatus = new System.Windows.Forms.Label();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // progressBarMain
@@ -54,7 +55,7 @@
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Image = ((System.Drawing.Image)(resources.GetObject("buttonCancel.InnerImage")));
+            this.buttonCancel.Image = ((System.Drawing.Image)(resources.GetObject("buttonCancel.Image")));
             this.buttonCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.buttonCancel.Location = new System.Drawing.Point(12, 64);
             this.buttonCancel.Name = "buttonCancel";
@@ -68,22 +69,36 @@
             // buttonSaveAs
             // 
             this.buttonSaveAs.Enabled = false;
-            this.buttonSaveAs.Image = ((System.Drawing.Image)(resources.GetObject("buttonSaveAs.InnerImage")));
+            this.buttonSaveAs.Image = ((System.Drawing.Image)(resources.GetObject("buttonSaveAs.Image")));
             this.buttonSaveAs.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.buttonSaveAs.Location = new System.Drawing.Point(95, 64);
             this.buttonSaveAs.Name = "buttonSaveAs";
-            this.buttonSaveAs.Size = new System.Drawing.Size(86, 28);
+            this.buttonSaveAs.Size = new System.Drawing.Size(96, 28);
             this.buttonSaveAs.TabIndex = 3;
             this.buttonSaveAs.Text = "Save as...";
             this.buttonSaveAs.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.buttonSaveAs.UseVisualStyleBackColor = true;
             this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
             // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(197, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Upload to Imgur";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormEmbeddingProgress
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(409, 103);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonSaveAs);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.labelStatus);
@@ -106,5 +121,6 @@
         private System.Windows.Forms.Label labelStatus;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.Button button1;
     }
 }
